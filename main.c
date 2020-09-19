66f38ba5ab2d81eb58d883b449551af9b597bfc9
jzk6139 <jzk6139@psu.edu>
2020-09-19 02:15:42 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+// author : Jonathan Kuldip jzk6139@psu.edu
+// collaborator : Justin Tien jst5381@psu.edu
+// collaborator : Kevin Chen khc5132@psu.edu
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello\n");
+  return 0;
+}
+// You may assume n >= 0, return the sum of 0+1+2+3+...+ n
+
+
+int sum_n(int n);
+
+
+
+
+
+
+
+// You may assume n >= 0, print s n times, each time in its own line.
+void print_n(const char *s, int n);
